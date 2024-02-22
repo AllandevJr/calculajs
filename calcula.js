3e25eac11ffc6f71c8112bf986161bef00452de3
Allandev <allanjeje@gmail.com>
2024-02-22 01:11:55 -0300

    Att em p/ imagens
    Foi alterado alguns pontos para que as imagens das formulas fossem mostradas ao executar, porém sem sucesso.

--- calcula.js
+++ calcula.js
@@ -8,3 +8,16 @@ var s = n1 + n2
 res.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${s}`
 }
 document.querySelector('input#saida') = res
+
+function forms(){
+    var img = document.querySelector('div#formula')
+        
+    if (n1 + n2){
+    img.src = 'soma.png'
+} else if(n1 - n2){
+    img.src = 'subtracao.png'
+} else if (n1 * n2){
+    img.src = 'multiplicacao.png'
+}else {
+    img.src = 'divisao.png'
+}}
